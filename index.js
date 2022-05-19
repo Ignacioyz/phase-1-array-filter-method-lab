6289b82b8b349921c64b7736ad4f708b94b35319
Ignacio Yz <ignacioyz@gmail.com>
2022-05-19 16:04:25 -0400

    commit tree 8b2c55f5138ca2309f4d086e6f017f3a4b988a82

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution here
+// function findMatching(driver, name){
+    
+//     return driver.filter(driver => name.toUpperCase() === driver.toUpperCase())
+
+// }
+
+// function fuzzyMatch(drivers, letters){
+//     return drivers.filter(driver => driver.indexOf(letters) === 0)
+// }
+
+// function matchName(drivers, name){
+//     return drivers.filter(driver => name === driver.name)
+
+// }
+
+
+
+
+
+function findMatching(array, string){
+  const newList = array.filter(drivers => drivers.toUpperCase() === string.toUpperCase())
+    return newList
+}
+
+function fuzzyMatch(array, string){
+    const firstLetter = array.filter(drivers => drivers.startsWith(string) )
+    return firstLetter 
+}
+
+function matchName(drvrObj, string){
+   return drvrObj.filter(drivers => drivers.name === string)
+    
+
+}
